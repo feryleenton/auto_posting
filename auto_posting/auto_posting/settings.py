4e5f377360ead9118ef918180149beccb2cbabdd
Your Name <you@example.com>
2021-07-21 14:35:40 +0300

    /posts cmd

--- auto_posting/auto_posting/settings.py
+++ auto_posting/auto_posting/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import sys
-sys.path.append('/home/admin/auto_posting')
+sys.path.append('/home/feryleeton/PycharmProjects/auto_posting')
 from data import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
